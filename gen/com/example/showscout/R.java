aa3eb734bd1a999a53adaf4d758d157f6a39294b
kkhat11 <kkhat111@gmail.com>
2014-11-12 01:37:57 -0600

    Bug Fixed: No more new activity error

--- gen/com/example/showscout/R.java
+++ gen/com/example/showscout/R.java
@@ -22,21 +22,43 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int customborder=0x7f020000;
+        public static final int greenbutton=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int microphone=0x7f020003;
+        public static final int showscoutfull=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080004;
+        public static final int editText1=0x7f080005;
+        public static final int imageView1=0x7f080003;
+        public static final int linearLayout1=0x7f080006;
+        public static final int playlist1=0x7f080007;
+        public static final int playlist2=0x7f080008;
+        public static final int playlist3=0x7f080009;
+        public static final int playlist4=0x7f08000a;
+        public static final int playlist5=0x7f08000b;
+        public static final int playlist6=0x7f08000c;
+        public static final int progressBar1=0x7f080001;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_loading=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int loading=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int load=0x7f050005;
+        public static final int loading=0x7f050004;
+        public static final int title_activity_loading=0x7f050003;
     }
     public static final class style {
         /** 
